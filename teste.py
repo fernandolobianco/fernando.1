0d1a8cdb5b1e0b55d62e399065ffcac897a5f045
fernandolobianco <fernandolobiancodesa@gmail.com>
2025-10-06 15:26:29 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title ("MEU PROGRAMA")
 st.write ("Alô mundo")
-seunome = text_input("Qual o seu nome?")
+seunome = st.text_input("Qual o seu nome?")
     if seunome: 
       st.write(seunome.upper())
       
