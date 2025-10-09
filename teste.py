c30f491b6b048d75946ea720e2a724b553b10a64
fernandolobianco <fernandolobiancodesa@gmail.com>
2025-10-09 20:45:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,27 +1,14 @@
 import streamlit as st
-import requests
 
-API_KEY = "SUA_API_KEY_AQUI"
-CX = "SEU_CX_AQUI"
-
-def buscar_imagem(query):
-    url = "https://www.googleapis.com/customsearch/v1"
-    params = {"key": API_KEY, "cx": CX, "q": query, "searchType": "image", "num": 1}
-    r = requests.get(url, params=params).json()
-    return r.get("items", [{}])[0].get("link")
-
-st.title("Medalha de São Bento")
-st.write("Fala, Josir!")
-
+st.title ("Medalha de São Bento")
+st.write ("Fala, Professor!")
 seunome = st.text_input("Qual o seu nome?")
-if seunome:
-    st.write(f"Seu nome é: {seunome.upper()}")
-
-resposta = st.text_input("Você gostaria de saber qual é a Medalha de São Bento? (sim/não):").strip().lower()
-
-if resposta == "sim":
-    imagem = buscar_imagem("Medalha de São Bento")
-    if imagem:
-        st.image(imagem, caption="Medalha de São Bento")
+seunome: 
+    st.write("Seu nome é: " seunome.upper())
+resposta = st.text_input("Você gostaria de saber qual é a Medalha de São Bento? (sim/não): ").strip().lower()  
+    if resposta == "sim":
+        imagemdamedalha = "https://reginafidei.com.br/wp-content/uploads/2025/07/Medalha-de-Sao-Bento.jpg"
+        st.image(imagemdamedalha, caption="Medalha de São Bento")
     else:
-        st.write("Não consegui encontrar a imagem.")
+        st.write ("Você jamais conhecerá a medalha mais bonita do mundo...")
+        
