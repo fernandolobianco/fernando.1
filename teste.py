2ef953b32b55ff7757badca1ee988b61ba06c084
fernandolobianco <fernandolobiancodesa@gmail.com>
2025-10-09 20:24:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,8 +2,13 @@ import streamlit as st
 
 st.title ("MEU PROGRAMA")
 st.write ("Alô mundo")
-
 seunome = st.text_input("Qual o seu nome?")
 if seunome: 
     st.write(seunome.upper())
-      
+resposta = st.text_input("Você gostaria de saber qual é a Medalha de São Bento? (sim/não): ").lower()  
+    if resposta == "sim":
+        imagem_url = buscar_imagem ("Medalha de São Bento")
+        print(imagem_url)
+        webbrowser.open(imagem_url)
+else:
+    print ("Não consegui encontrar a imagem.")
