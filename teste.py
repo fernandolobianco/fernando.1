e066135c6abb8105eac5ca5d0bac801a97009f2d
fernandolobianco <fernandolobiancodesa@gmail.com>
2025-10-09 20:35:52 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,15 @@
-import streamlit as st
+st.title("Medalha de São Bento")
+st.write("Fala, Josir!")
 
-st.title ("MEU PROGRAMA")
-st.write ("Alô mundo")
 seunome = st.text_input("Qual o seu nome?")
-if seunome: 
-    st.write(seunome.upper())
-resposta = st.text_input("Você gostaria de saber qual é a Medalha de São Bento? (sim/não): ").lower()  
-    if resposta == "sim":
-        imagem_url = buscar_imagem ("Medalha de São Bento")
-        print(imagem_url)
-        webbrowser.open(imagem_url)
-else:
-    print ("Não consegui encontrar a imagem.")
+if seunome:
+    st.write(f"Seu nome é: {seunome.upper()}")
+
+resposta = st.text_input("Você gostaria de saber qual é a Medalha de São Bento? (sim/não):").strip().lower()
+
+if resposta == "sim":
+    imagem = buscar_imagem("Medalha de São Bento")
+    if imagem:
+        st.image(imagem, caption="Medalha de São Bento")
+    else:
+        st.write("Não consegui encontrar a imagem.")
